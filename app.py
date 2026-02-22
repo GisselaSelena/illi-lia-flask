970b2829f7ca8d87675aa6c8058f70b069fba74c
GisselaSelena <gs.medinac@uea.edu.ec>
2026-02-22 08:37:58 -0500

    Semana 10: plantillas con herencia y verificación local

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/producto/<nombre_producto>')
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/producto/<nombre_producto>")
 def producto(nombre_producto):
     nombre_formateado = nombre_producto.replace('-', ' ').title()
     return render_template("producto.html", nombre=nombre_formateado)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
