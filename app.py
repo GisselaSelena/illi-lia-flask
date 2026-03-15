fcfd63195bdad6fc4844936f21bc4171c70d807c
Lia Salome <liasalome@Lias-MacBook-Pro.local>
2026-03-14 22:15:43 -0500

    CRUD completo usuarios MySQL Flask

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from conexion.conexion import obtener_conexion
 import os
 
 # importar funciones de persistencia
@@ -24,7 +25,7 @@ def producto(nombre_producto):
     return render_template("producto.html", nombre=nombre_formateado)
 
 
-# NUEVA RUTA para mostrar los datos guardados
+# Mostrar datos TXT JSON CSV
 @app.route("/datos")
 def ver_datos():
 
@@ -40,6 +41,90 @@ def ver_datos():
     )
 
 
+# VER USUARIOS DESDE MYSQL
+@app.route("/usuarios")
+def usuarios():
+
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    cursor.execute("SELECT * FROM usuarios")
+
+    usuarios = cursor.fetchall()
+
+    conexion.close()
+
+    return render_template("usuarios.html", usuarios=usuarios)
+
+
+# AGREGAR USUARIO MYSQL
+@app.route("/agregar_usuario", methods=["GET", "POST"])
+def agregar_usuario():
+
+    if request.method == "POST":
+
+        nombre = request.form["nombre"]
+        mail = request.form["mail"]
+        password = request.form["password"]
+
+        conexion = obtener_conexion()
+        cursor = conexion.cursor()
+
+        cursor.execute(
+            "INSERT INTO usuarios (nombre, mail, password) VALUES (%s,%s,%s)",
+            (nombre, mail, password)
+        )
+
+        conexion.commit()
+        conexion.close()
+
+        return redirect("/usuarios")
+
+    return render_template("agregar_usuario.html")
+
+@app.route("/eliminar_usuario/<int:id>")
+def eliminar_usuario(id):
+
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    cursor.execute("DELETE FROM usuarios WHERE id_usuario = %s", (id,))
+
+    conexion.commit()
+    conexion.close()
+
+    return redirect("/usuarios")
+
+@app.route("/editar_usuario/<int:id>", methods=["GET", "POST"])
+def editar_usuario(id):
+
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    if request.method == "POST":
+
+        nombre = request.form["nombre"]
+        mail = request.form["mail"]
+        password = request.form["password"]
+
+        cursor.execute(
+            "UPDATE usuarios SET nombre=%s, mail=%s, password=%s WHERE id_usuario=%s",
+            (nombre, mail, password, id)
+        )
+
+        conexion.commit()
+        conexion.close()
+
+        return redirect("/usuarios")
+
+    cursor.execute("SELECT * FROM usuarios WHERE id_usuario=%s", (id,))
+    usuario = cursor.fetchone()
+
+    conexion.close()
+
+    return render_template("editar_usuario.html", usuario=usuario)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
